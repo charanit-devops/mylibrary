a8347f3efed00e71385ab7fac481a70ff93e5d40
charanit-devops <95111912+charanit-devops@users.noreply.github.com>
2025-07-29 11:23:28 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -1,1 +1,20 @@
 
+def sum(a,b)
+{
+  echo "The addition of ${a} and ${b} is {a+b}"
+}
+
+def sub(a,b)
+{
+  echo "The subtraction of ${a} and ${b} is {a-b}"
+}
+
+def multiplication(a,b)
+{
+  echo "The multiplication of ${a} and ${b} is {a*b}"
+}
+
+def division(a,b)
+{
+  echo "The division of ${a} and ${b} is {a/b}"
+}
